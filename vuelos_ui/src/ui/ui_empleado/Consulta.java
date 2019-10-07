530f6e1a927c97ffbcf2e86f7b6e78fff2d6686e
carloza <carlos.i.loza@gmail.com>
2019-10-07 20:47:31 -0300

    seguimos con ui_empleado

--- vuelos_ui/src/ui/ui_empleado/Consulta.java
+++ vuelos_ui/src/ui/ui_empleado/Consulta.java
@@ -3,6 +3,8 @@ package ui.ui_empleado;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -17,7 +19,11 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.Types;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
@@ -142,6 +148,7 @@ public class Consulta {
 		tablaIDA = new DBTable();
 		tablaIDA.setBounds(12, 94, 772, 340);
 		tablaIDA.setEditable(false);
+		tablaIDA.addMouseListener(new TablaListener(tablaIDA));
 		frame.getContentPane().add(tablaIDA);
 		
 		lblVuelosDeVuelta = new JLabel("Vuelos de vuelta");
@@ -152,7 +159,48 @@ public class Consulta {
 		tablaVUELTA = new DBTable();
 		tablaVUELTA.setBounds(12, 277, 772, 157);
 		tablaVUELTA.setEditable(false);
+		tablaIDA.addMouseListener(new TablaListener(tablaVUELTA));
 		frame.getContentPane().add(tablaVUELTA);
+	}
+	
+	private void rowSeleccionada(DBTable tabla) {
+		try {
+			String vuelo = tabla.getValueAt(tabla.getSelectedRow(), 0).toString();
+			String fecha = tabla.getValueAt(tabla.getSelectedRow(), 1).toString();
+			
+			//hago todo este lio xq retorna un dia menos que el almacenado en bd
+			//https://stackoverflow.com/questions/428918/how-can-i-increment-a-date-by-one-day-in-java
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+			Calendar c = Calendar.getInstance();
+			c.setTime(sdf.parse(fecha));
+			c.add(Calendar.DATE, 1);  // number of days to add
+			fecha = sdf.format(c.getTime());  // dt is now the new date
+			//aca termina el lio
+			
+			String query = "SELECT DISTINCT vd.fecha,vd.clase, vd.precio, vd.asientos_disponibles "
+						 + "FROM vuelos.vuelos_disponibles AS vd "
+						 + "WHERE (vd.vuelo = "+vuelo+") AND (vd.fecha = '"+fecha+"');";
+			Statement stmt = vuelos_db.get_Connection_Vuelos_DB().createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+			String disponibles = "Para el vuelo "+vuelo+" del dia "+fecha+" ofrece: \n";
+			while(rs.next()) {
+				String clase = rs.getString("clase");
+				String precio = rs.getString("precio");
+				String asientos = rs.getString("asientos_disponibles");
+				String oracion = "La clase "+clase+" cuesta $"+precio+" y quedan "+asientos+" asientos disponibles \n";
+				disponibles = disponibles + oracion;
+			}
+			JOptionPane.showMessageDialog(frame,
+                    disponibles,
+                    "Vuelos para "+fecha,
+                    JOptionPane.INFORMATION_MESSAGE);
+//			System.out.println(disponibles);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		
 	}
 	
 	private void inflarTablas() {
@@ -178,6 +226,14 @@ public class Consulta {
 			tabla_db.setConnection(vuelos_db.get_Connection_Vuelos_DB());
 			tabla_db.setSelectSql(query);
 			tabla_db.createColumnModelFromQuery();
+			for (int i = 0; i < tabla_db.getColumnCount(); i++) { // para que muestre correctamente los valores de tipo TIME (hora)  		   		  
+	    		 if	 (tabla_db.getColumn(i).getType()==Types.TIME){    		 
+	    			 tabla_db.getColumn(i).setType(Types.CHAR);  
+	  	       	 }
+	    		 if	 (tabla_db.getColumn(i).getType()==Types.DATE){
+	    			 tabla_db.getColumn(i).setDateFormat("dd/MM/YYYY");
+	    		 }
+	          }
 			tabla_db.refresh();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -187,7 +243,7 @@ public class Consulta {
 	
 	protected String generarQuery(Ubicacion origen, Ubicacion destino) {
 		String s = "select distinct "
-				+ "vd.vuelo, vd.nombre_ap_salida, vd.hora_sale, vd.nombre_ap_llegada, "
+				+ "vd.vuelo, vd.fecha, vd.nombre_ap_salida, vd.hora_sale, vd.nombre_ap_llegada, "
 				+ "vd.hora_llega, vd.modelo_avion, vd.tiempo_estimado "
 				+ "from vuelos_disponibles as vd "
 				+ "where (vd.ciudad_ap_salida = '"+origen.getCiudad()+"') and "
@@ -198,6 +254,33 @@ public class Consulta {
 					+ "(vd.pais_ap_llegada = '"+destino.getPais()+"')";
 //		System.out.println(s);
 		return s;
+	}
+	
+	private class TablaListener implements MouseListener{
+		
+		private DBTable tablaLocal;
+		
+		public TablaListener(DBTable tablaLocal) {
+			tablaLocal = this.tablaLocal;
+		}
+
+		@Override
+		public void mouseClicked(MouseEvent arg0) {
+			rowSeleccionada(tablaLocal);
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent arg0) {}
+
+		@Override
+		public void mouseExited(MouseEvent arg0) {}
+
+		@Override
+		public void mousePressed(MouseEvent arg0) {}
+
+		@Override
+		public void mouseReleased(MouseEvent arg0) {}
+		
 	}
 	
 	private class Boton_ver_listener implements ActionListener {
